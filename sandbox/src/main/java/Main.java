f8035cc82878c27f857daca9e3d6256a0e464395
Mikhail Materukhin <9226343456@mail.ru>
2019-10-01 23:11:45 +0500

    initial commit

---
+++ sandbox/src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    static Point p1 = new Point(2, 2);
+    static Point p2 = new Point(5, 6);
+
+    public static void main(String[] args) {
+        System.out.println(Point.distance(p1, p2));
+    }
+}
---
+++ sandbox/src/main/java/Point.java
@@ -0,0 +1,20 @@
+public class Point {
+    private int x, y;
+
+    private int getX() {
+        return x;
+    }
+
+    private int getY() {
+        return y;
+    }
+
+    Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public static double distance(Point p1, Point p2) {
+        return Math.sqrt(Math.pow((p2.getX() - p1.getX()), 2) + Math.pow((p2.getY() - p1.getY()), 2));
+    }
+}
